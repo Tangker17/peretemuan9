07055e5fe4d0cbdc99d140be826adec522fa5b1b
Ramonchucaltex17 <92871153+Tangker17@users.noreply.github.com>
2021-11-30 00:09:45 +0700

    Add files via upload

---
+++ latihan5.py
@@ -0,0 +1,47 @@
+nilai = [11,12,13,14,15]
+print (nilai)
+#akses list
+print ("tampilkan element ke 3 : ",nilai[2])
+print ("ambil element 2 dan 4 : ", nilai[1:4])
+print ("ambil element terakhir : ", nilai[4])
+#ubah element
+print ("===========================")
+print ("ubah element")
+nilai [3] = 17
+print ("ubah element ke 4 dengan nilai lainya: ", nilai[0:5])
+nilai [3:5] = 20,21
+print("ubah element ke 4 sampai dengan element terakhir:",nilai[0:5])
+print ("===========================")
+nilaia = [11,12,13,14,15]
+nilaib = [16,17,18,19,20]
+print("list a : ", nilaia)
+print("list b : ", nilaib)
+sama = (nilai+nilaib)
+nilaia=[11,12,13]
+print("===========================")
+nilaib=[14,15,16,17,18,19,20]
+print("nilai a = ",nilaia)
+print("nilai b =",nilaib)
+print("===========================")
+print("tambah list b dengan nilai string")
+dataa=["ramon",17,"01"]
+datab=[31,20,10,50]
+datab.append(60)
+print(datab)
+print ("menambahkan list b nilai string 3 ",datab)
+datab.extend([31,21,10])
+print("menambahkan list b dengan 3 nilai", datab)
+print ("menggabungkan list a dan list b ", dataa+datab)
+print("=============================")
+
+
+
+
+
+
+
+
+
+
+
+
